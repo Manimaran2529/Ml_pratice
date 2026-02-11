42e8bd14a72420bb2b0fef703c00c026f2a03184
manimaran <manimaranh9965@gmail.com>
2026-02-11 22:05:01 +0530

    elif

---
+++ CONDITIONS/elif.py
@@ -0,0 +1,11 @@
+mani=(input("enter a number"))
+if(mani>='A' and mani<='Z'):
+    print("captital" )
+elif(mani>='a' and mani<='z'):
+    print("small")
+elif(mani=="!","@","#","$","%","^","&*"):
+    print("special")
+else:
+    print("numbers")
+
+    
